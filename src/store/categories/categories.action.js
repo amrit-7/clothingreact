0042f796f16590df13b28f66ed9545c197bcf6ff
amrit <gillamrit29@gmail.com>
2023-02-10 21:52:13 +0530

    Redux-Saga to fetch categories

--- src/store/categories/categories.action.js
+++ src/store/categories/categories.action.js
@@ -1,6 +1,5 @@
 import { CATEGORIES_ACTION_TYPES } from "./categories.types";
 import { createAction } from "../../utils/reducer/reducer.utils";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 export const fetchCategoriesSuccess = (categoriesArray) =>
   createAction(
     CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
@@ -10,15 +9,3 @@ export const fetchCategoriesStart = (categoriesArray) =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, categoriesArray);
 export const fetchCategoriesFailed = (error) =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
-
-// Thunk Function //
-
-export const fetchCategoriesAsync = () => async (dispatch) => {
-  dispatch(fetchCategoriesStart());
-  try {
-    const categoriesArray = await getCategoriesAndDocuments();
-    dispatch(fetchCategoriesSuccess(categoriesArray));
-  } catch (error) {
-    dispatch(fetchCategoriesFailed(error));
-  }
-};
--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,11 @@ import logger from "redux-logger";
 import { rootReducer } from "./root-reducer/rootreducer";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
+import createSagaMiddleware from "@redux-saga/core";
+import { rootSaga } from "./root-saga";
+
+const sagaMiddelware = createSagaMiddleware();
 
 const persistConfig = {
   key: "root",
@@ -15,9 +19,10 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: [process.env.NODE_ENV !== "production" && logger, thunk].filter(
-    Boolean
-  ),
+  middleware: [
+    process.env.NODE_ENV !== "production" && logger,
+    sagaMiddelware,
+  ].filter(Boolean),
   devTools: true,
 });
 export const persistor = persistStore(store);

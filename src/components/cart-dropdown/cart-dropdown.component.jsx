606cdb6e47354a585160b5563d79542073969186
amrit <gillamrit29@gmail.com>
2023-02-15 16:14:24 +0530

    commit

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,34 +1,34 @@
-import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-
 import { selectCartItems } from "../../store/cart/cart.selector";
-import Button from "../button/button.component";
+import { Button } from "react-bootstrap";
 import CartItem from "../cart-item/cart-item.component";
-
+import { useNavigate } from "react-router";
 import {
   CartDropdownContainer,
+  Cartitem,
   EmptyMessage,
-  CartItems,
 } from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
-  const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
 
-  const goToCheckoutHandler = () => {
+  const checkout = (e) => {
+    e.preventDefault();
     navigate("/checkout");
   };
-
+  const cartItems = useSelector(selectCartItems);
   return (
     <CartDropdownContainer>
-      <CartItems>
+      <Cartitem>
         {cartItems.length ? (
           cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
         ) : (
           <EmptyMessage>Your cart is empty</EmptyMessage>
         )}
-      </CartItems>
-      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+      </Cartitem>
+      <Button className="cart-btn" onClick={checkout}>
+        Checkout
+      </Button>
     </CartDropdownContainer>
   );
 };
